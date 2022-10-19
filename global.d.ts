528d7eb1110ffbd9366f80405895607a73571eba
Nastasios <nastasiosg@gmail.com>
2022-10-19 10:50:49 +0200

    Fixed the global.d.ts for Cosmos Vue.js

--- global.d.ts
+++ global.d.ts
@@ -1,34 +1,20 @@
 /// <reference types="svelte" />
 /// <reference types="preact.d.ts" />
-declare module '*.vue' {
-  // NOTE: ts-loader
-  import { defineComponent } from 'vue'
 
-  const component: ReturnType<typeof defineComponent>
-  export default component
-}
-
-import type { JSX } from "@cosmos/web";
+import type { JSX } from '@cosmos/web'
 // Add Intrinsic Elements to allow Cosmos custom elements tag names to be used in JSX
-type LocalIntrinsicElements = JSX.IntrinsicElements;
+type LocalIntrinsicElements = JSX.IntrinsicElements
 
 declare global {
-  namespace JSX {
-    interface IntrinsicElements extends LocalIntrinsicElements {}
-    declare module '*.jpeg';
-    declare module '*.png';
-    declare module '*.jpg';
-    declare module "*.svg" {
-      const content: any;
-      export default content;
-    }
-  }
-}
-
-declare module '*.jpeg';
-declare module '*.png';
-declare module '*.jpg';
-declare module "*.svg" {
-  const content: any;
-  export default content;
+	namespace JSX {
+		interface IntrinsicElements extends LocalIntrinsicElements {}
+		declare module '*.jpeg'
+		declare module '*.png'
+		declare module '*.jpg'
+		declare module '*.vue'
+		declare module '*.svg' {
+			const content: any
+			export default content
+		}
+	}
 }
